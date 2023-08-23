f1f7a53c06ae249d7c106dbec52e6f966eebc890
Waterdish <waterdish101@gmail.com>
2023-08-22 19:04:02 -0700

    Open up all storage permissions to read all files

--- app/src/main/java/com/dishii/zelda3/MainActivity.java
+++ app/src/main/java/com/dishii/zelda3/MainActivity.java
@@ -1,21 +1,80 @@
 
 package com.dishii.zelda3;
 import org.libsdl.app.SDLActivity;
+
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.Settings;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.FileOutputStream;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import android.util.Log;
 
 //This class is the main SDLActivity and just sets up a bunch of default files
-public class MainActivity extends SDLActivity {
+public class MainActivity extends SDLActivity{
+
+
+    private static final int STORAGE_PERMISSION_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // Check if storage permissions are granted
+        if (hasStoragePermission()) {
+            setupFiles();
+        } else {
+            requestStoragePermission();
+        }
+    }
+
+        // Check if storage permission is granted
+        private boolean hasStoragePermission() {
+            return ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED &&
+                    ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                            == PackageManager.PERMISSION_GRANTED;
+        }
+
+        // Request storage permission
+        private void requestStoragePermission() {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    STORAGE_PERMISSION_REQUEST_CODE);
+
+            try {
+                Uri uri = Uri.parse("package:" + BuildConfig.APPLICATION_ID);
+                Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION, uri);
+                startActivity(intent);
+            } catch (Exception ex){
+                Intent intent = new Intent();
+                intent.setAction(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
+                startActivity(intent);
+            }
+        }
+
+        // Handle permission request result
+        @Override
+        public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            if (requestCode == STORAGE_PERMISSION_REQUEST_CODE) {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    setupFiles();
+                } else {
+                    // Permission denied, handle accordingly (e.g., show a message)
+                }
+            }
+        }
+
+    private void setupFiles() {
         // Check if external storage is available
         if (isExternalStorageWritable()) {
             // Get the root directory of the external storage
